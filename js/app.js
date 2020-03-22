fa13f9aef112d39f370e490660c92fc5810b181a
Tony Vithayathil <tony.vithayathil@gmail.com>
2020-03-21 20:52:38 -0400

    added functionality to navbar and scroll to top button

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
+const sectionList = document.querySelectorAll('section');
 const scrollToTopButton = document.querySelector('#scroll_to_top_btn');
+const navBar = document.querySelector('#navbar');
 
-// Below code is based on tutorial from https://www.youtube.com/watch?v=FK5DEa1Hvco
+// Dynamically create navbar
+(function makeNavBar() {
+	const unorderedList = document.createElement('ul');
+
+	for (i = 0; i < sectionList.length; i++) {
+		const listItems = document.createElement('li');
+		const createLink = document.createElement('a');
+		createLink.setAttribute('href', `#${sectionList[i].id}`);
+		let linkName = sectionList[i].id;
+		let removeUnderscore = linkName.toUpperCase().replace(/_/g, ' ');
+
+		createLink.innerText = removeUnderscore;
+		listItems.append(createLink);
+		unorderedList.append(listItems);
+		console.log(createLink);
+	}
+	unorderedList.setAttribute('class', 'nav_link_container');
+	navBar.append(unorderedList);
+})();
+
+// scroll button
 scrollToTopButton.addEventListener('click', function() {
 	window.scrollTo({
 		top: 0,
@@ -9,11 +31,14 @@ scrollToTopButton.addEventListener('click', function() {
 	});
 });
 
-// Below code is based on tutorial from https://www.youtube.com/watch?v=gphMli74Chk
-window.addEventListener('scroll', ifPageIsScrolling);
+window.addEventListener('scroll', function() {
+	ifPageIsScrolling();
+	showNavBar();
+});
 
+// show scrollToTopButton -- based on tutorial from https://www.youtube.com/watch?v=gphMli74Chk
 function ifPageIsScrolling() {
-	if (window.pageYOffset > 1200) {
+	if (window.pageYOffset > 1100) {
 		if (!scrollToTopButton.classList.contains('btn_enter')) {
 			scrollToTopButton.classList.remove('btn_exit');
 			scrollToTopButton.classList.add('btn_enter');
@@ -27,5 +52,29 @@ function ifPageIsScrolling() {
 		setTimeout(function() {
 			scrollToTopButton.style.display = 'none';
 		}, 400);
+	}
+}
+
+const innerNavContianer = document.querySelector('.nav_link_container');
+// when to show the nav bar
+function showNavBar() {
+	if (window.innerWidth > 600) {
+		innerNavContianer.style.display = 'flex';
+		if (window.pageYOffset > 142) {
+			navBar.style.top = 0;
+		} else {
+			navBar.style.top = '-50px';
+		}
+	}
+}
+
+// hide navbar on mobile until clicked
+function hideNavBar() {
+	if (window.innerWidth <= 600) {
+		if (innerNavContianer.style.display === 'none') {
+			innerNavContianer.style.display = 'flex';
+		} else {
+			innerNavContianer.style.display = 'none';
+		}
 	}
 }
